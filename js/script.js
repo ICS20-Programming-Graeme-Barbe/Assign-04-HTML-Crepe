674d2ee4c071f38f69be8299e5046b8ba8f51d01
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-05-09 17:25:29 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Created by Graeme Barbe
+// Created on: May 9
+// JS functions for index.html
+
+
+// This function tells the user the cost of getting into the museum
+'use strict';
+function submit() {
+	// Declares variables
+	let select = document.getElementById('number')
+	let numberOfCrêpes = select.options[select.selectedIndex].value
+	let selectToppings = document.getElementById('topping')
+	let numberOfToppings = selectToppings.options[select.selectedIndex].value
+
+	// If statement to determine number of crêpes selected 
+	if (numberOfCrêpes == "")
+		//Nothing was inputted 
+		document.getElementById('total').innerHTML = "Please select a size"
+	else if (numberOfCrêpes == "one")
+		let numberPrice = 3
+}
